82b7a49d07f90324d1a8b43ee1d03131078cb4ad
nuberchardzer <nuberchardzer@gmail.com>
2026-02-01 22:55:59 +0000

    wip: minimal working implementation

--- src/zxc/_zxc.c
+++ src/zxc/_zxc.c
@@ -1,118 +1,307 @@
 #define PY_SSIZE_T_CLEAN
 
-#include <Python.h>
 #include "zxc.h"
+#include <Python.h>
+
+#define Py_Return_Errno(err)                                                   \
+    do {                                                                       \
+        PyErr_SetFromErrno(err);                                               \
+        return NULL;                                                           \
+    } while (0)
+#define Py_Return_Err(err, str)                                                \
+    do {                                                                       \
+        PyErr_SetString(err, str);                                             \
+        return NULL;                                                           \
+    } while (0)
 
 // =============================================================================
 // Wrapper functions
 // =============================================================================
 
-static PyObject* pyzxc_compress(PyObject* self, PyObject* args, PyObject* kwargs);
-static PyObject* pyzxc_decompress(PyObject* self, PyObject* args, PyObject* kwargs);
-static PyObject* pyzxc_stream_compress(PyObject* self, PyObject* args, PyObject* kwargs);
-static PyObject* pyzxc_stream_decompress(PyObject* self, PyObject* args, PyObject* kwargs);
+static PyObject *pyzxc_compress(PyObject *self, PyObject *args,
+                                PyObject *kwargs);
+static PyObject *pyzxc_decompress(PyObject *self, PyObject *args,
+                                  PyObject *kwargs);
+static PyObject *pyzxc_stream_compress(PyObject *self, PyObject *args,
+                                       PyObject *kwargs);
+static PyObject *pyzxc_stream_decompress(PyObject *self, PyObject *args,
+                                         PyObject *kwargs);
 
 // =============================================================================
 // Initialize python module
 // =============================================================================
 
-PyDoc_STRVAR(
-    zxc_doc,
-    "ZXC bindings.\n"
-    "\n"
-    "API:\n"
-    "  compress(data, level=5, checksum=False) -> bytes\n"
-    "  decompress(data, checksum=False) -> bytes\n"
-    "  stream_compress(src, dst, level=5, checksum=False) -> None\n"
-    "  stream_decompress(src, dst, checksum=False) -> None\n"
-);
+PyDoc_STRVAR(zxc_doc,
+             "ZXC bindings.\n"
+             "\n"
+             "API:\n"
+             "  compress(data, level=5, checksum=False) -> bytes\n"
+             "  decompress(data, checksum=False) -> bytes\n"
+             "  stream_compress(src, dst, level=5, checksum=False) -> None\n"
+             "  stream_decompress(src, dst, checksum=False) -> None\n");
 
-PyDoc_STRVAR(
-    compress_doc,
-    "compress(data, level=5, checksum=False) -> bytes\n"
-    "Compress a bytes-like object (buffer protocol).\n"
-);
+PyDoc_STRVAR(compress_doc, "compress(data, level=5, checksum=False) -> bytes\n"
+                           "Compress a bytes-like object (buffer protocol).\n");
 
-PyDoc_STRVAR(
-    decompress_doc,
-    "decompress(data, checksum=False) -> bytes\n"
-    "Decompress ZXC-compressed data.\n"
-    "Raises RuntimeError on invalid input.\n"
-);
+PyDoc_STRVAR(decompress_doc, "decompress(data, checksum=False) -> bytes\n"
+                             "Decompress ZXC-compressed data.\n"
+                             "Raises RuntimeError on invalid input.\n");
 
-PyDoc_STRVAR(
-    stream_compress_doc,
-    "stream_compress(src, dst, level=5, checksum=False) -> None\n"
-    "Compress from src to dst (file-like or path).\n"
-);
+PyDoc_STRVAR(stream_compress_doc,
+             "stream_compress(src, dst, level=5, checksum=False) -> None\n"
+             "Compress from src to dst (file-like or path).\n");
 
-PyDoc_STRVAR(
-    stream_decompress_doc,
-    "stream_decompress(src, dst, checksum=False) -> None\n"
-    "Decompress from src to dst (file-like or path).\n"
-);
+PyDoc_STRVAR(stream_decompress_doc,
+             "stream_decompress(src, dst, checksum=False) -> None\n"
+             "Decompress from src to dst (file-like or path).\n");
 
 static PyMethodDef zxc_methods[] = {
-    {
-        "compress",
-        (PyCFunction)pyzxc_compress,
-        METH_VARARGS | METH_KEYWORDS,
-        compress_doc
-    },
-    {
-        "decompress",
-        (PyCFunction)pyzxc_decompress,
-        METH_VARARGS | METH_KEYWORDS,
-        decompress_doc
-    },
-    {
-        "stream_compress",
-        (PyCFunction)pyzxc_stream_compress,
-        METH_VARARGS | METH_KEYWORDS,
-        stream_compress_doc
-    },
-    {
-        "stream_decompress",
-        (PyCFunction)pyzxc_stream_decompress,
-        METH_VARARGS | METH_KEYWORDS,
-        stream_decompress_doc
-    },
-    {NULL, NULL, 0, NULL}  // sentinel
+    {"compress", (PyCFunction)pyzxc_compress, METH_VARARGS | METH_KEYWORDS,
+     compress_doc},
+    {"decompress", (PyCFunction)pyzxc_decompress, METH_VARARGS | METH_KEYWORDS,
+     decompress_doc},
+    {"stream_compress", (PyCFunction)pyzxc_stream_compress,
+     METH_VARARGS | METH_KEYWORDS, stream_compress_doc},
+    {"stream_decompress", (PyCFunction)pyzxc_stream_decompress,
+     METH_VARARGS | METH_KEYWORDS, stream_decompress_doc},
+    {NULL, NULL, 0, NULL} // sentinel
 };
 
-static struct PyModuleDef zxc_module = {
-    PyModuleDef_HEAD_INIT,
-    "_zxc",   
-    zxc_doc,     
-    0,       
-    zxc_methods
-};
+static struct PyModuleDef zxc_module = {PyModuleDef_HEAD_INIT, "_zxc", zxc_doc,
+                                        0, zxc_methods};
 
-PyMODINIT_FUNC
-PyInit__zxc(void){
-    return PyModuleDef_Init(&zxc_module);
-}
+PyMODINIT_FUNC PyInit__zxc(void) { return PyModuleDef_Init(&zxc_module); }
 
 // =============================================================================
 // Functions definitions
 // =============================================================================
 
-static PyObject* pyzxc_compress(PyObject* self, PyObject* args, PyObject* kwargs) {
-    PyErr_SetString(PyExc_NotImplementedError, "compress not implemented yet");
-    return NULL;
+static PyObject *pyzxc_compress(PyObject *self, PyObject *args,
+                                PyObject *kwargs) {
+    Py_buffer view;
+    int level = ZXC_LEVEL_DEFAULT;
+    int checksum = 0;
+
+    static char *kwlist[] = {"data", "level", "checksum", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y*|ip", kwlist, &view,
+                                     &level, &checksum)) {
+        return NULL;
+    }
+
+    if (view.itemsize != 1) {
+        PyBuffer_Release(&view);
+        PyErr_SetString(PyExc_TypeError,
+                        "expected a byte buffer (itemsize==1)");
+        return NULL;
+    }
+
+    size_t src_size = (size_t)view.len;
+
+    size_t bound = zxc_compress_bound(src_size);
+
+    PyObject *out = PyBytes_FromStringAndSize(NULL, (Py_ssize_t)bound);
+    if (!out) {
+        PyBuffer_Release(&view);
+        return NULL;
+    }
+
+    char *dst = PyBytes_AsString(out); // Return a pointer to the contents
+    size_t n_write;                    // The number of bytes written to dst
+
+    Py_BEGIN_ALLOW_THREADS
+    n_write = zxc_compress(view.buf, // Source buffer
+                            src_size, // Source size
+                            dst,      // Destination buffer
+                            bound,    // Destination capacity
+                            level,    // Compression level
+                            checksum  // Checksum
+    );
+    Py_END_ALLOW_THREADS
+
+    PyBuffer_Release(&view);
+
+    if (n_write == 0) {
+        Py_DECREF(out);
+        PyErr_SetString(PyExc_RuntimeError, "zxc_compress failed");
+        return NULL;
+    }
+
+    if (_PyBytes_Resize(&out, (Py_ssize_t)n_write) < 0) // Realloc
+        return NULL;                                 
+
+    return out;
 }
 
-static PyObject* pyzxc_decompress(PyObject* self, PyObject* args, PyObject* kwargs) {
-    PyErr_SetString(PyExc_NotImplementedError, "decompress not implemented yet");
-    return NULL;
+static PyObject *pyzxc_decompress(PyObject *self, PyObject *args,
+                                  PyObject *kwargs) {
+    Py_buffer view;
+    int checksum = 0;
+
+    Py_ssize_t original_size;
+    static char *kwlist[] = {"data", "original_size", "checksum", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y*n|p", kwlist, &view,
+                                     &original_size, &checksum)) {
+        return NULL;
+    }
+
+    if (view.itemsize != 1) {
+        PyBuffer_Release(&view);
+        PyErr_SetString(PyExc_TypeError,
+                        "expected a byte buffer (itemsize==1)");
+        return NULL;
+    }
+
+    size_t src_size = (size_t)view.len;
+
+    PyObject *out = PyBytes_FromStringAndSize(NULL, (Py_ssize_t)original_size);
+    if (!out) {
+        PyBuffer_Release(&view);
+        return NULL;
+    }
+
+    char *dst = PyBytes_AsString(out); // Return a pointer to the contents
+    size_t nwritten;                   // The number of bytes written to dst
+
+    Py_BEGIN_ALLOW_THREADS
+    nwritten = zxc_decompress(view.buf,      // Source buffer
+                                src_size,      // Source size
+                                dst,           // Destination buffer
+                                original_size, // Destination capacity
+                                checksum       // Verify checksum
+    );
+    Py_END_ALLOW_THREADS
+
+    PyBuffer_Release(&view);
+
+    if (nwritten == 0) {
+        Py_DECREF(out);
+        PyErr_SetString(PyExc_RuntimeError, "zxc_decompress failed");
+        return NULL;
+    }
+
+    return out;
 }
 
-static PyObject* pyzxc_stream_compress(PyObject* self, PyObject* args, PyObject* kwargs) {
-    PyErr_SetString(PyExc_NotImplementedError, "stream compress not implemented yet");
-    return NULL;
+static PyObject *pyzxc_stream_compress(PyObject *self, PyObject *args,
+                                       PyObject *kwargs) {
+    PyObject *src, *dst;
+    int nthreads = 0;
+    int level = ZXC_LEVEL_DEFAULT;
+    int checksum = 0;
+
+    static char *kwlist[] = {"src",   "dst",      "n_threads",
+                             "level", "checksum", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OO|iip", kwlist, &src, &dst,
+                                     &nthreads, &level, &checksum)) {
+        return NULL;
+    }
+
+    int src_fd = PyObject_AsFileDescriptor(src);
+    int dst_fd = PyObject_AsFileDescriptor(dst);
+
+    if (src_fd == -1 || dst_fd == -1)
+        Py_Return_Err(PyExc_RuntimeError, "couldn't get file descriptor");
+
+    int src_dup = dup(src_fd);
+    if (src_dup == -1) {
+        Py_Return_Errno(PyExc_OSError);
+    }
+    int dst_dup = dup(dst_fd);
+    if (dst_dup == -1) {
+        close(src_dup);
+        Py_Return_Errno(PyExc_OSError);
+    }
+
+    // TODO: check fd flags
+
+    FILE *fsrc = fdopen(src_dup, "rb");
+    if (!fsrc) {
+        close(src_dup);
+        close(dst_dup);
+        Py_Return_Errno(PyExc_OSError);
+    }
+
+    FILE *fdst = fdopen(dst_dup, "wb");
+    if (!fdst) {
+        fclose(fsrc);
+        close(dst_dup);
+        Py_Return_Errno(PyExc_OSError);
+    }
+
+    int nwritten;
+
+    Py_BEGIN_ALLOW_THREADS 
+    nwritten = zxc_stream_compress(fsrc, fdst, nthreads, level, checksum);
+    Py_END_ALLOW_THREADS
+
+    fclose(fdst);
+    fclose(fsrc);
+
+    if (nwritten < 0)
+        Py_Return_Err(PyExc_RuntimeError, "zxc_stream_compress failed");
+
+    Py_RETURN_NONE;
 }
 
-static PyObject* pyzxc_stream_decompress(PyObject* self, PyObject* args, PyObject* kwargs) {
-    PyErr_SetString(PyExc_NotImplementedError, "stream decompress not implemented yet");
-    return NULL;
+static PyObject *pyzxc_stream_decompress(PyObject *self, PyObject *args,
+                                         PyObject *kwargs) {
+    PyObject *src, *dst;
+    int nthreads = 0;
+    int checksum = 0;
+
+    static char *kwlist[] = {"src", "dst", "n_threads", "checksum", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OO|ip", kwlist, &src, &dst,
+                                     &nthreads, &checksum)) {
+        return NULL;
+    }
+
+    int src_fd = PyObject_AsFileDescriptor(src);
+    int dst_fd = PyObject_AsFileDescriptor(dst);
+
+    if (src_fd == -1 || dst_fd == -1)
+        Py_Return_Err(PyExc_RuntimeError, "couldn't get file descriptor");
+
+    int src_dup = dup(src_fd);
+    if (src_dup == -1) {
+        Py_Return_Errno(PyExc_OSError);
+    }
+    int dst_dup = dup(dst_fd);
+    if (dst_dup == -1) {
+        close(src_dup);
+        Py_Return_Errno(PyExc_OSError);
+    }
+
+    // TODO: check fd flags
+
+    FILE *fsrc = fdopen(src_dup, "rb");
+    if (!fsrc) {
+        close(src_dup);
+        close(dst_dup);
+        Py_Return_Errno(PyExc_OSError);
+    }
+
+    FILE *fdst = fdopen(dst_dup, "wb");
+    if (!fdst) {
+        fclose(fsrc);
+        close(dst_dup);
+        Py_Return_Errno(PyExc_OSError);
+    }
+
+    int nwritten;
+
+    Py_BEGIN_ALLOW_THREADS 
+    nwritten = zxc_stream_decompress(fsrc, fdst, nthreads, checksum);
+    Py_END_ALLOW_THREADS
+
+    fclose(fdst);
+    fclose(fsrc);
+
+    if (nwritten < 0)
+        Py_Return_Err(PyExc_RuntimeError, "zxc_stream_decompress failed");
+
+    Py_RETURN_NONE;
 }
